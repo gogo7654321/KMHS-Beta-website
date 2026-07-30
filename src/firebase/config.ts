8143df8764d845c224bcc983de8c65be7195af95
AP-Ace <npatel012010@gmail.com>
2026-07-30 13:54:20 -0400

    commit firebase config so app hosting build can resolve it

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+// Firebase web app configuration.
+// This file is gitignored (see .gitignore) as sensitive configuration.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAM8UvIJVMBONoElzR8rlWiGTqN-SupXOE",
+  authDomain: "studio-8652128073-34949.firebaseapp.com",
+  projectId: "studio-8652128073-34949",
+  storageBucket: "studio-8652128073-34949.firebasestorage.app",
+  messagingSenderId: "995999291856",
+  appId: "1:995999291856:web:8c9095806b561bd831c1d5",
+};
